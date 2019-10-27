888bcd9b8c6214071acf3ceef3ade0990c87fa67
Thomas Krause <thomaskrause@posteo.de>
2019-10-27 18:39:42 +0100

    Extend existing YAML file and use a proper YAML serializer

--- src/main/java/org/corpus_tools/cffmaven/CreateFromDependenciesMojo.java
+++ src/main/java/org/corpus_tools/cffmaven/CreateFromDependenciesMojo.java
@@ -1,42 +1,38 @@
 package org.corpus_tools.cffmaven;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.LinkedList;
 import java.util.List;
-
-import com.github.mustachejava.DefaultMustacheFactory;
-import com.github.mustachejava.Mustache;
-import com.github.mustachejava.MustacheFactory;
+import java.util.Map;
 
 import org.apache.maven.artifact.Artifact;
-import org.apache.maven.artifact.DefaultArtifact;
 import org.apache.maven.artifact.repository.ArtifactRepository;
 import org.apache.maven.execution.MavenSession;
-import org.apache.maven.model.Dependency;
 import org.apache.maven.model.Developer;
 import org.apache.maven.model.License;
 import org.apache.maven.model.building.ModelBuildingRequest;
-import org.apache.maven.project.ProjectBuildingRequest;
-import org.apache.maven.project.ProjectBuildingResult;
-import org.apache.maven.project.DefaultProjectBuildingRequest;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugins.annotations.Component;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.plugins.annotations.ResolutionScope;
-import org.apache.maven.project.DefaultProjectBuilderConfiguration;
+import org.apache.maven.project.DefaultProjectBuildingRequest;
 import org.apache.maven.project.MavenProject;
-import org.apache.maven.project.MavenProjectBuilder;
 import org.apache.maven.project.ProjectBuilder;
 import org.apache.maven.project.ProjectBuildingException;
-import org.codehaus.plexus.component.annotations.Requirement;
+import org.apache.maven.project.ProjectBuildingRequest;
+import org.apache.maven.project.ProjectBuildingResult;
+import org.snakeyaml.engine.v2.api.Dump;
+import org.snakeyaml.engine.v2.api.DumpSettings;
+import org.snakeyaml.engine.v2.api.Load;
+import org.snakeyaml.engine.v2.api.LoadSettings;
+import org.snakeyaml.engine.v2.common.FlowStyle;
 
 /**
  * Create Citation File Format with references from the dependencies defined via
@@ -52,7 +48,10 @@ public class CreateFromDependenciesMojo extends AbstractMojo {
     private MavenProject project;
 
     @Parameter(defaultValue = "${basedir}/CITATION.cff")
-    private File citationFile;
+    private File output;
+
+    @Parameter(defaultValue = "")
+    private File input;
 
     @Parameter(defaultValue = "${project.remoteArtifactRepositories}", readonly = true)
     protected List<ArtifactRepository> remoteRepositories;
@@ -65,19 +64,25 @@ public class CreateFromDependenciesMojo extends AbstractMojo {
 
     public void execute() throws MojoExecutionException {
 
-        getLog().info("maven session: " + mavenSession);
-        getLog().info("project builder: " + mavenProjectBuilder);
+        LoadSettings yamlLoadSettings = LoadSettings.builder().build();
+        Load yamlLoad = new Load(yamlLoadSettings);
+        Map<String, Object> cff = new LinkedHashMap<>();
+        if(input != null && input.isFile()) {
+            try(FileInputStream inputFile = new FileInputStream(input)) {
+                yamlLoad.loadFromInputStream(inputFile);
+            } catch (Throwable ex) {
+                getLog().error("Error loading input YAML file " + input.getPath(), ex);
+            }
+        }
 
         ProjectBuildingRequest projectBuildingRequest = new DefaultProjectBuildingRequest(
                 mavenSession.getProjectBuildingRequest()).setRemoteRepositories(remoteRepositories)
                         .setValidationLevel(ModelBuildingRequest.VALIDATION_LEVEL_MINIMAL).setResolveDependencies(false)
                         .setProcessPlugins(false);
 
-        HashMap<String, Object> scopes = new HashMap<>();
-
         // set basic properties like title
-        scopes.put("title", project.getName());
-        scopes.put("version", project.getVersion());
+        cff.putIfAbsent("title", project.getName());
+        cff.putIfAbsent("version", project.getVersion());
 
         List<HashMap<String, Object>> authors = new LinkedList<>();
         for (Developer dev : project.getModel().getDevelopers()) {
@@ -85,7 +90,7 @@ public class CreateFromDependenciesMojo extends AbstractMojo {
             author.put("name", dev.getName());
             authors.add(author);
         }
-        scopes.put("author", authors);
+        cff.putIfAbsent("authors", authors);
 
         // collect references to add
         List<HashMap<String, Object>> references = new LinkedList<>();
@@ -115,18 +120,16 @@ public class CreateFromDependenciesMojo extends AbstractMojo {
             }
 
         }
-        scopes.put("reference", references);
+        cff.put("reference", references);
 
-        // Write out the file using the template and the collected information
-        MustacheFactory mustacheFactory = new DefaultMustacheFactory();
-        try (InputStream cff_template = getClass().getResourceAsStream("citation_template.yaml");
-                FileWriter outWriter = new FileWriter(citationFile)) {
-
-            Mustache m = mustacheFactory.compile(new InputStreamReader(cff_template, StandardCharsets.UTF_8), "CFF");
-            m.execute(outWriter, scopes);
-
+        // Write out the YAML file again
+        DumpSettings dumpSettings = DumpSettings.builder().setDefaultFlowStyle(FlowStyle.BLOCK).build();
+        Dump dumpYaml = new Dump(dumpSettings);
+        try (FileWriter outWriter = new FileWriter(output)) {
+            String yamlAsString = dumpYaml.dumpToString(cff);
+            outWriter.write(yamlAsString);
         } catch (IOException ex) {
-            getLog().error("Could create Citation file from template", ex);
+            getLog().error("Could not write Citation file", ex);
         }
 
     }
