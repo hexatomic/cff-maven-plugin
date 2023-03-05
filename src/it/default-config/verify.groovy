0c6157598b795541985221c85ed9681a3aa60f58
Thomas Krause <thomaskrause@posteo.de>
2023-03-05 23:34:03 +0100

    Add comparison for CITATION.cff files

--- src/it/default-config/verify.groovy
+++ src/it/default-config/verify.groovy
@@ -1,8 +1,7 @@
-File file = new File( basedir, "CITATION.cff" );
-if ( !file.isFile() )
-{
-    throw new FileNotFoundException( "Could not find generated CITATION.cff file: " + file );
-}
+File actualFile = new File( basedir, "CITATION.cff" );
+File expectedFile = new File("src/it/default-config/CITATION.cff")
+
+org.corpus_tools.cffmaven.FileDiff.compare(expectedFile, actualFile);
 
 File folder = new File( basedir, "THIRD-PARTY" );
 if ( !folder.isDirectory() )
