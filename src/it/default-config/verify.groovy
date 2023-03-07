a5d5d0b911ef8b200e4ade81c0424ad7e78cd9fd
Thomas Krause <thomaskrause@posteo.de>
2023-03-07 13:21:52 +0100

    Use BeanShell script for verification

--- src/it/default-config/verify.groovy
+++
@@ -1,8 +0,0 @@
-File expectedFile = new File("src/it/default-config/CITATION.cff")
-File actualFile = new File( basedir, "CITATION.cff" );
-
-org.corpus_tools.cffmaven.FileDiff.compare(expectedFile, actualFile);
-
-File expectedFolder = new File("src/it/default-config/THIRD-PARTY")
-File actualFolder = new File( basedir, "THIRD-PARTY" );
-org.corpus_tools.cffmaven.FileDiff.compare(expectedFolder, actualFolder, false);
